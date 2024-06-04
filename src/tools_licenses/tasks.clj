6f24228feaa572e21eb7373aec5e1ab2fe904a27
Peter Monks <pmonks@gmail.com>
2024-06-04 16:16:41 -0700

    :new: Improve counting of single- vs multi- licensed dependencies

--- src/tools_licenses/tasks.clj
+++ src/tools_licenses/tasks.clj
@@ -126,13 +126,14 @@
 (defn- summary-output!
   "Emit summary output to stdout."
   [proj-expressions-info deps-lib-map-with-info]
-  (let [proj-expressions     (sort-license-expressions (keys proj-expressions-info))
-        freqs                (frequencies (filter identity (mapcat #(keys (get % :lice-comb/license-info)) (vals deps-lib-map-with-info))))
-        deps-expressions     (sort-license-expressions (keys freqs))
-        license-infos        (map #(:lice-comb/license-info (val %)) deps-lib-map-with-info)
-        no-license-count     (count (filter empty? license-infos))
-        single-license-count (count (filter #(and (= 1 (count (keys %))) (sexp/simple? (first (keys %)))) license-infos))
-        multi-license-count  (count (filter #(or (> (count (keys %)) 1) (some sexp/compound? (keys %))) license-infos))]
+  (let [proj-expressions             (sort-license-expressions (keys proj-expressions-info))
+        freqs                        (frequencies (filter identity (mapcat #(keys (get % :lice-comb/license-info)) (vals deps-lib-map-with-info))))
+        deps-expressions             (sort-license-expressions (keys freqs))
+        license-infos                (map #(:lice-comb/license-info (val %)) deps-lib-map-with-info)
+        no-license-count             (count (filter empty? license-infos))
+        single-license-count         (count (filter #(and (= 1 (count (keys %))) (sexp/simple? (first (keys %)))) license-infos))
+        multi-license-count          (count (filter #(or (> (count (keys %)) 1) (some sexp/compound? (keys %))) license-infos))
+        multiple-exprs-license-count (count (filter #(> (count (keys %)) 1) license-infos))]
     (print (str "\n" (ansi/bold "This project: ")))
     (if (seq proj-expressions)
       (println (s/join ", " (map human-readable-expression proj-expressions)))
@@ -149,6 +150,7 @@
                   "\n                             " (ansi/bold "Deps with no detected licenses: ") (fit-width 9 (str no-license-count) false)
                   "\n                                        " (ansi/bold "Deps with 1 license: ") (fit-width 9 (str single-license-count) false)
                   "\n                                " (ansi/bold "Deps with multiple licenses: ") (fit-width 9 (str multi-license-count) false)
+                  "\n                     " (ansi/bold "Deps with multiple license expressions: ") (fit-width 9 (str multiple-exprs-license-count) false)
                   "\n                                                 " (ansi/bold "TOTAL DEPS: "  (fit-width 9 (str (count deps-lib-map-with-info)) false))
                   "\n"))))
 
